02c06186eed22648d7a4b2bec26e1b9d88cffa52
Philip <luethiphilip@gmail.com>
2024-06-18 09:05:05 +0200

    shoot right direction

--- Shootergame_1v1_code.py
+++ Shootergame_1v1_code.py
@@ -6,6 +6,7 @@ WIDTH, HEIGHT = 1000, 500
 FPS = 60
 GRAVITY = 1
 JUMP_SPEED = 15
+BULLET_COOLDOWN = 1000 / 5
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -29,6 +30,8 @@ class PLAYER:
         self.x_vel = 5
         self.y_vel = 0
         self.jump_count = 0
+        self.bullets = []
+        self.last_shot = pygame.time.get_ticks()
 
 
     def draw(self):
@@ -38,12 +41,10 @@ class PLAYER:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.player_rect.x -= self.x_vel
+            self.direction = "left"
         if keys[pygame.K_RIGHT]:
             self.player_rect.x += self.x_vel
-
-    def gravity(self):
-        self.y_vel += GRAVITY
-        self.player_rect.y += self.y_vel
+            self.direction = "right"
 
     def jump(self):
         keys = pygame.key.get_pressed()
@@ -52,7 +53,9 @@ class PLAYER:
         if keys[pygame.K_UP] and self.jump_count < 1:
                 self.y_vel = -JUMP_SPEED
                 self.jump_count += 1
-
+    def gravity(self):
+        self.y_vel += GRAVITY
+        self.player_rect.y += self.y_vel
 
     def collide_vertical(self, blocks):
         self.touching_ground = False
@@ -71,12 +74,45 @@ class PLAYER:
                 elif self.player_rect.left < block.block_rect.right and self.player_rect.right > block.block_rect.right:
                     self.player_rect.left = block.block_rect.right
 
+    def shoot(self):
+        keys = pygame.key.get_pressed()
+        current_time = pygame.time.get_ticks()
+        if keys[pygame.K_SPACE] and current_time - self.last_shot > BULLET_COOLDOWN:
+            bullet_x = self.player_rect.centerx
+            bullet_y = self.player_rect.centery
+            new_bullet = BULLET(bullet_x, bullet_y)
+            self.bullets.append(new_bullet)
+            self.last_shot = current_time
+
+
     def update(self):
         self.move_horizontaly()
         self.gravity()
         self.collide_vertical(blocks)
         self.collide_horizontal(blocks)
         self.jump()
+        self.shoot()
+        for bullet in self.bullets:
+            bullet.update()
+
+
+class BULLET:
+    COLOR = pygame.Color("yellow")
+    SPEED = 10
+    WIDTH, HEIGHT = 10, 5
+    DISTANCE = 60
+
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, self.WIDTH, self.HEIGHT)
+
+    def draw(self):
+        pygame.draw.rect(screen, self.COLOR, self.rect)
+
+
+    def update(self):
+        self.rect.x += self.SPEED
+        self.draw()
+
 
 def draw_elements(blocks, player):
     player.draw()
@@ -88,6 +124,9 @@ blocks = [
     BLOCK(200, 450, 500, 50),
     BLOCK(300, 400, 200, 50)
 ]
+bullet = BULLET
+
+
 running = True
 
 while running:
