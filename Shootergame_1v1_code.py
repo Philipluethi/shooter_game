ad58bde95051faf10c8de356d0285be914567f35
Philip <luethiphilip@gmail.com>
2024-07-08 17:20:23 +0200

    BULLET class in separate file

--- Shootergame_1v1_code.py
+++ Shootergame_1v1_code.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from block import BLOCK
+from bullet import BULLET
 
 pygame.init()
 
@@ -142,39 +143,8 @@ class PLAYER:
         self.gravity()
         self.collide_vertical(blocks)
         for bullet in self.bullets:
-            bullet.update()
+            bullet.update(screen, player_1, player_2)
         self.check_lives()
-
-class BULLET:
-    COLOR = pygame.Color("black")
-    SPEED = 10
-    WIDTH, HEIGHT = 10, 5
-
-    def __init__(self, x, y, direction, player_number):
-        self.rect = pygame.Rect(x, y, self.WIDTH, self.HEIGHT)
-        self.direction = direction
-        self.bullet_number = player_number
-        self.collided = False
-
-    def draw(self):
-        pygame.draw.rect(screen, self.COLOR, self.rect)
-
-    def collide_player(self, player_1, player_2):
-        if self.rect.colliderect(player_1.rect) and self.bullet_number == 2:
-            self.collided = True
-            player_1.lives -= 1
-        if self.rect.colliderect(player_2.rect) and self.bullet_number == 1:
-            self.collided = True
-            player_2.lives -= 1
-
-    def update(self):
-        if self.collided == False:
-            if self.direction == "right":
-                self.rect.x += self.SPEED
-            elif self.direction == "left":
-                self.rect.x -= self.SPEED
-            self.draw()
-            self.collide_player(player_1, player_2)
 
 class RANDOM_MAP:
 
---
+++ bullet.py
@@ -0,0 +1,32 @@
+import pygame
+
+class BULLET:
+    COLOR = pygame.Color("black")
+    SPEED = 10
+    WIDTH, HEIGHT = 10, 5
+
+    def __init__(self, x, y, direction, player_number):
+        self.rect = pygame.Rect(x, y, self.WIDTH, self.HEIGHT)
+        self.direction = direction
+        self.bullet_number = player_number
+        self.collided = False
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.COLOR, self.rect)
+
+    def collide_player(self, player_1, player_2):
+        if self.rect.colliderect(player_1.rect) and self.bullet_number == 2:
+            self.collided = True
+            player_1.lives -= 1
+        if self.rect.colliderect(player_2.rect) and self.bullet_number == 1:
+            self.collided = True
+            player_2.lives -= 1
+
+    def update(self, screen, player_1, player_2):
+        if self.collided == False:
+            if self.direction == "right":
+                self.rect.x += self.SPEED
+            elif self.direction == "left":
+                self.rect.x -= self.SPEED
+            self.draw(screen)
+            self.collide_player(player_1, player_2)
