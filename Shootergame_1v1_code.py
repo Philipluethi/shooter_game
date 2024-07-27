7ca1d977ee3beb37461b1ddf6befce385b820897
Philip <luethiphilip@gmail.com>
2024-07-27 21:18:51 +0200

    main class perfected

--- Shootergame_1v1_code.py
+++ Shootergame_1v1_code.py
@@ -18,12 +18,10 @@ JUMP_SPEED = 15
 JUMP_LIMIT = 2
 BULLET_COOLDOWN = 1000 / 10
 
-
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 pygame.display.update()
 pygame.display.set_caption("Shooter Game 1v1")
-
 
 
 class MAIN:
@@ -39,7 +37,7 @@ class MAIN:
     def print(self):
         pass
 
-    def update_elements(self, blocks, player_1, player_2):
+    def update_elements(self):
         player_1.update(GRAVITY, blocks, screen, player_1, player_2, SCREEN_W, SCREEN_H)
         player_2.update(GRAVITY, blocks, screen, player_1, player_2, SCREEN_W, SCREEN_H)
         item.update(screen, player_1, player_2, PLAYER_W, PLAYER_H, BULLET_W, BULLET_H)
@@ -48,13 +46,13 @@ class MAIN:
             block.draw(screen)
             if keys[pygame.K_i] or item.rect.colliderect(block.rect):
                 item.reroll_pos(SCREEN_W, SCREEN_H)
-# F-String from Vid 3
+        # F-String from Vid 3
         self.draw_text(f"P1: {player_1.lives}", self.lives_font, (0, 0, 0), SCREEN_W - 100, 50)
         self.draw_text(f"P2: {player_2.lives}", self.lives_font, (0, 0, 0), 100, 50)
 
     def check_keys(self):
         keys = pygame.key.get_pressed()
-# P1
+        # P1
         if keys[pygame.K_LEFT]:
             player_1.move_left()
 
@@ -62,7 +60,7 @@ class MAIN:
             player_1.move_right()
 
         if keys[pygame.K_UP]:
-            if player_1.jump_pressed == False and player_1.jump_count < JUMP_LIMIT:
+            if not player_1.jump_pressed and player_1.jump_count < JUMP_LIMIT:
                 player_1.jump(JUMP_SPEED)
                 player_1.jump_count += 1
                 player_1.jump_pressed = True
@@ -73,12 +71,11 @@ class MAIN:
         if keys[pygame.K_SPACE]:
             player_1.shoot(BULLET, BULLET_COOLDOWN)
 
-        if player_1.touching_ground == True:
+        if player_1.touching_ground:
             if keys[pygame.K_DOWN]:
                 player_1.dodge_ground = True
 
-
-# P2
+        # P2
 
         if keys[pygame.K_a]:
             player_2.move_left()
@@ -87,7 +84,7 @@ class MAIN:
             player_2.move_right()
 
         if keys[pygame.K_w]:
-            if player_2.jump_pressed == False and player_2.jump_count < JUMP_LIMIT:
+            if not player_2.jump_pressed and player_2.jump_count < JUMP_LIMIT:
                 player_2.jump(JUMP_SPEED)
                 player_2.jump_count += 1
                 player_2.jump_pressed = True
@@ -98,11 +95,11 @@ class MAIN:
         if keys[pygame.K_CAPSLOCK]:
             player_2.shoot(BULLET, BULLET_COOLDOWN)
 
-        if player_2.touching_ground == True:
+        if player_2.touching_ground:
             if keys[pygame.K_s]:
                 player_2.dodge_ground = True
 
-# MAP
+        # MAP
         if keys[pygame.K_r]:
             blocks[1:] = []
             main.random_map()
@@ -117,17 +114,14 @@ class MAIN:
             self.winner = 1
             self.game_over = True
 
-
-
     def random_map(self):
         for col in range(SCREEN_H // BLOCK_H):
-            if col % random.randint(1,2) == 0:
+            if col % random.randint(1, 2) == 0:
                 for row in range(SCREEN_W // BLOCK_W):
                     if row % random.randint(1, 5) == 0:
                         blocks.append(BLOCK(row * BLOCK_H, col * BLOCK_H, BLOCK_W, BLOCK_H))
 
-
-# FROM VID 3
+    # FROM VID 3
     def draw_text(self, text, font, color, center_x, center_y):
         text_img = font.render(text, True, color)
         text_rect = text_img.get_rect(center=(center_x, center_y))
@@ -137,13 +131,12 @@ class MAIN:
 # INSTANZEN
 
 main = MAIN()
-player_1 = PLAYER(1, 500, 0,  PLAYER_W, PLAYER_H, BULLET_W, BULLET_H)
-player_2 = PLAYER(2, 200, 0,  PLAYER_W, PLAYER_H, BULLET_W, BULLET_H)
+player_1 = PLAYER(1, 500, 0, PLAYER_W, PLAYER_H, BULLET_W, BULLET_H)
+player_2 = PLAYER(2, 200, 0, PLAYER_W, PLAYER_H, BULLET_W, BULLET_H)
 item = ITEM_BOX(SCREEN_W, SCREEN_H)
 blocks = [
     BLOCK(0, SCREEN_H - BLOCK_H, SCREEN_W, BLOCK_H)
 ]
-
 
 # GAME LOOPS
 
@@ -153,11 +146,12 @@ one_player = False
 main.random_map()
 
 while start_screen:
+    keys = pygame.key.get_pressed()
+
     screen.fill(pygame.Color("white"))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             start_screen = False
-    keys = pygame.key.get_pressed()
     if keys[pygame.K_2]:
         print("2-PLAYER")
         start_screen = False
@@ -170,7 +164,8 @@ while start_screen:
 
     main.draw_text("CHOOSE A GAME MODE", main.title_font, pygame.Color("black"), SCREEN_W // 2, SCREEN_H // 2 - 150)
     main.draw_text("press 1 for single player", main.subtitle_font, pygame.Color("black"), SCREEN_W // 2, SCREEN_H // 2)
-    main.draw_text("press 2 for two-player", main.subtitle_font, pygame.Color("black"), SCREEN_W // 2, SCREEN_H // 2 + 50)
+    main.draw_text("press 2 for two-player", main.subtitle_font, pygame.Color("black"), SCREEN_W // 2,
+                   SCREEN_H // 2 + 50)
 
     clock.tick(FPS)
     pygame.display.update()
@@ -180,14 +175,12 @@ while two_player:
         if event.type == pygame.QUIT:
             two_player = False
     screen.fill(pygame.Color("light blue"))
-    if main.game_over == True:
+    if main.game_over:
         two_player = False
 
-
-    main.update_elements(blocks, player_1, player_2)
+    main.update_elements()
     main.check_keys()
     main.check_lives()
-
 
     pygame.display.update()
     clock.tick(FPS)
@@ -218,21 +211,17 @@ while main.game_over:
             main.game_over = False
 
     if main.winner == 1:
-        game_over_color = pygame.Color("red")
-        winner_color = "RED"
+        winner_color = "red"
         player_1.update(GRAVITY, blocks, screen, player_1, player_2, SCREEN_W, SCREEN_H)
 
-
     if main.winner == 2:
-        game_over_color = pygame.Color("blue")
-        winner_color = "BLUE"
+        winner_color = "blue"
         player_2.update(GRAVITY, blocks, screen, player_1, player_2, SCREEN_W, SCREEN_H)
 
-    main.draw_text(f"Player {winner_color} wins", main.title_font, game_over_color, SCREEN_W // 2, SCREEN_H // 2 - 50)
-    main.draw_text("GAME OVER", main.subtitle_font, game_over_color, SCREEN_W // 2, SCREEN_H // 2)
+    main.draw_text(f"Player {main.winner} wins", main.title_font, pygame.Color(winner_color), SCREEN_W // 2, SCREEN_H // 2 - 50)
+    main.draw_text("GAME OVER", main.subtitle_font, pygame.Color(winner_color), SCREEN_W // 2, SCREEN_H // 2)
 
     pygame.display.update()
     clock.tick(FPS)
-
 
 pygame.quit()
